53fe5a22bbe399746182e2369b1398df5e182f66
Mphatso Lichapa <lichapau@gmail.com>
2022-06-09 12:11:33 +0200

    Created hello react front end

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MessageContainer from './components/MessageContainer';
+
+function App() {
+  return (
+    <div>
+      <h1>A simple greeting App</h1>
+      <MessageContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MessageContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class MessageContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    axios
+      .get('http://localhost:3000/api/v1/messages')
+      .then((res) => {
+        this.setState({ messages: res.data });
+      });
+  }
+
+  render() {
+    const { messages } = this.state;
+    return (
+      <div>
+        <div className="taskContainer" />
+        <div className="wrapItems">
+          <ul className="listItems">
+            {messages.slice(0, 1).map((message) => (
+              <li className="item" message={message} key={message.id}>
+                <span className="itemDisplay">{message.greeting}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+export default MessageContainer;
